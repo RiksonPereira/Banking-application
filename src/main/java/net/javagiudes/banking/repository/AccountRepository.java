ad898988c6f22b3407237bd6a58864c26f90f6b3
rikson <riksonpereira22@gmail.com>
2024-11-27 12:54:04 +0530

    Revert "Creating Banking Application. Rest API calls"
    This reverts commit cd25995b085240c8369d2b78a2706a28ae1c4d29.

--- src/main/java/net/javagiudes/banking/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javagiudes.banking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.javagiudes.banking.entity.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-}
